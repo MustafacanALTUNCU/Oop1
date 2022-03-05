b9471e29ee5bd84b3a26bdbb0613c4453cd090f9
MustafacanALTUNCU <mustafacanaltuncu0@gmail.com>
2022-03-06 01:03:30 +0300

    Add project files.

---
+++ Oop1/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop1
+{
+    internal class ProductManager
+    {
+        public void Add(Product product)
+
+        {
+            Console.WriteLine(product.ProductName + "eklehndi");
+
+        }
+        public void Upgrade(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi");
+        }
+        public int Topla1(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+    }
+}
---
+++ Oop1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Oop1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStock = 5, ProductName = "kalem", UnitPrice = 35 };
+            ProductManager productManager= new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+            productManager.Upgrade(product1);
+            productManager.Topla2(5, 6);
+            int sonuc = productManager.Topla1(5, 6);
+            Console.WriteLine(sonuc*2);
+        }
+    }
+}
